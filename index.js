5205d6bc5e0a7b554bdce0e641a9297d18b047f4
Sai Ranga Pavan <121741348+Bathalapalli-SaiRangaPavan@users.noreply.github.com>
2023-03-12 20:03:17 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Valaxy Technologies nodejs!');
+  res.send('Welcome to DevOps World!');
 });
 
 var server = app.listen(3000, function () {
